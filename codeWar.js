cf46cc6cfa16bbf831c383e958417cbe7d54803b
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-04 09:48:40 +0530

    Practice

---
+++ codeWar.js
@@ -0,0 +1,86 @@
+// // 1./Write a function "greet" that returns "hello world!"
+// function greet (){
+//     return "hello world!"
+//   }
+
+
+//   //2 function DNAtoRNA(dna) {
+// //   // create a function which returns an RNA sequence from the given DNA sequence
+// //   return dna.replaceAll("T","U")
+// // }
+//  let DNAtoRNA=(dna)=> dna.replaceAll("T","U")
+ 
+
+
+// //3.function paperwork(n, m) {
+//    if(n>=0 && m>=0){
+//     return n*m
+//   }
+//   else{
+//     return 0;
+//   }
+ 
+
+
+// //4.if even number then mult with 8 and if not mult with odd
+// let simpleMultiplication=(number)=>number%2==0 ? number*8:number*9
+
+
+// //5.Convert number to reversed array of digits,  35231 => [1,3,2,5,3]
+// function digitize(n) {
+//     return Array.from(String(n)).map(Number).reverse()
+//   }
+
+
+//   //6. transform a number (integer) into a string. 123  --> "123"
+//   function numberToString(num) {
+//     // Return a string of the number here!
+//     return num.toString()
+//   }
+
+
+// //7. he drinks 0.5 litres of water per hour of cycling. ,time = 6.7---> litres = 3 , rounded to the smallest value.
+// function litres(time) {
+//     return Math.floor(time*0.5)
+//   }
+
+
+// //8.Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers..
+// var countSheep = function (num){
+//     let result=""
+//     for(let i=1;i<=num;i++){
+      
+//       result += `${i} sheep...`;
+//     }
+//     return result;
+//   }
+
+
+// //9.Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// function evenOrOdd(number) {
+//     return number%2==0 ? "Even":"Odd"
+//   }
+
+
+// //10.Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// /* 
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]  The correct answer would be 17.
+// */
+// function countSheeps(sheep) {
+//     // TODO
+//     return sheep.filter(Boolean).length //IT WILL RETURN COUNT OF ONLY TRUE
+//   }
+
+
+// //11.friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"] ONLY RETURN 4 LETTER WORDS
+// function friend(friends){
+//     //your code here
+//     return friends.filter((value) => value.length==4)
+//   }
+  
+
