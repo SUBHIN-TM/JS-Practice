0db65ddfd7bf695c8cd28b29729a863f80e237a8
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-07 14:08:52 +0530

    18 and 19

--- codeWar.js
+++ codeWar.js
@@ -1,3 +1,5 @@
+
+
 // // 1./Write a function "greet" that returns "hello world!"
 // function greet (){
 //     return "hello world!"
@@ -86,46 +88,46 @@
 
 
 //12.just print the positive numbers
-let a=[1, 5.2, 4, 0, -1] 
-function display(a){
-const result=a.filter((data) => data > 0);
-console.log(result);
-}
-display(a)
-console.log("\n");
+// let a=[1, 5.2, 4, 0, -1] 
+// function display(a){
+// const result=a.filter((data) => data > 0);
+// console.log(result);
+// }
+// display(a)
+// console.log("\n");
 
 
 
 ////13.Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
-let b=[1, 5.2, 4, 0, -1] //OP SHOULD BE 9.2
+// let b=[1, 5.2, 4, 0, -1] //OP SHOULD BE 9.2
 
-function sum(x){
-    if(x.length==0){
-        return 0
-    }
-    return x.reduce((total,number)=> total + number,0)
+// function sum(x){
+//     if(x.length==0){
+//         return 0
+//     }
+//     return x.reduce((total,number)=> total + number,0)
 
-}
-console.log(sum(b));
-console.log("\n");
+// }
+// console.log(sum(b));
+// console.log("\n");
 
 
 
 /*14.You get an array of numbers, return the sum of all of the positives ones.
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.*/
-var c=[1,-4,7,12]
- function add(x){
-    return x.reduce((total,number) => {
-        if(number >= 0){
-           return total + number;
-        }else{
-            return total
-        }
-    },0)
- }
-console.log(add(c));
-console.log("\n");
+// var c=[1,-4,7,12]
+//  function add(x){
+//     return x.reduce((total,number) => {
+//         if(number >= 0){
+//            return total + number;
+//         }else{
+//             return total
+//         }
+//     },0)
+//  }
+// console.log(add(c));
+// console.log("\n");
 
 
  /*15.ask:Given a list of integers, determine whether the sum of its elements is odd or even.
@@ -133,12 +135,12 @@ Give your answer as a string matching "odd" or "even".
 If the input array is empty consider it as: [0] (array with a zero). */
 
 
-console.log(oddOrEven([]));
-function oddOrEven(array) {
-    const result =array.reduce((total,data) => total + data,0)
-    return array.length == 0 ? "even": result %2 ==0 ? "even":"odd";
- }
- console.log("\n");
+// console.log(oddOrEven([]));
+// function oddOrEven(array) {
+//     const result =array.reduce((total,data) => total + data,0)
+//     return array.length == 0 ? "even": result %2 ==0 ? "even":"odd";
+//  }
+//  console.log("\n");
 
 
  /*16.
@@ -146,47 +148,67 @@ makeNegative(1);    // return -1
 makeNegative(-5);   // return -5
 makeNegative(0);    // return 0
 makeNegative(0.12); // return -0.12 */
-const check = (x) => x<0?x:0-x
-console.log(check(0));
-console.log("\n");
+
+// const check = (x) => x<0?x:0-x
+// console.log(check(0));
+// console.log("\n");
 
 
 /*17. You are going to be given a word. Your job is to return the middle character of the word. 
 If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters. */
-// function getMiddle(s)
-// {
-//    let array= s.split("")
-//    let index=Math.floor(array.length/2)
-//    if(array.length%2!=0){
-//     return array[index]
-//    }else{
-//     return array.slice(index,index +2).join('')
-//    }
-  
-// }
 
-// console.log(getMiddle("haah"));
-
-function getMiddle(s) {
-    let array = s.split("");
-    let length = Math.floor(array.length / 2);
+// function getMiddle(s) {
+//     let array = s.split("");
+//     let length = Math.floor(array.length / 2);
   
-    if (array.length % 2 !== 0) {
-      return array[length];
-    } else {
-      return array.slice(length -1, length + 2).join('');//length value -1 get the crct index slice(index and 1 extra to slice next)
+//     if (array.length % 2 !== 0) {
+//       return array[length];
+//     } else {
+//       return array.slice(length -1, length + 2).join('');//length value -1 get the crct index slice(index and 1 extra to slice next)
+//     }
+//   }
+  
+//   console.log(getMiddle("haah"));
+  
+
+
+
+  
+
+  
+  /*18.Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+  */
+
+var summation = function (num) {
+    let k=0;
+    for(let i=0;i<=num;i++) {
+    k += i
     }
+   return k
   }
-  
-  console.log(getMiddle("haah"));
-  
+  console.log(summation(2));
   
   
   
   
-  
-  
+  /*18.
+  Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+*/
 
 
+function abbrevName(name){
+  let k= name.split(" ")  //while we put 2 space it store as 2 words if we put 1 space it wil store as strins as all letters seperatlty
+  let firstName=k[0].charAt(0).toUpperCase()
+  let lastName=k[1].charAt(0).toUpperCase()
+  return `${firstName}.${lastName};`
+}
+console.log(abbrevName("subhin Akshaya"));
  
+
 
