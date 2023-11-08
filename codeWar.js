5e019bec37d97182e219e70fa075e9f646e712b3
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-08 20:56:22 +0530

    20

--- codeWar.js
+++ codeWar.js
@@ -181,19 +181,19 @@ If the word's length is odd, return the middle character. If the word's length i
 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
   */
 
-var summation = function (num) {
-    let k=0;
-    for(let i=0;i<=num;i++) {
-    k += i
-    }
-   return k
-  }
-  console.log(summation(2));
+// var summation = function (num) {
+//     let k=0;
+//     for(let i=0;i<=num;i++) {
+//     k += i
+//     }
+//    return k
+//   }
+//   console.log(summation(2));
   
   
   
   
-  /*18.
+  /*19.
   Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 The output should be two capital letters with a dot separating them.
 It should look like this:
@@ -202,13 +202,26 @@ patrick feeney => P.F
 */
 
 
-function abbrevName(name){
-  let k= name.split(" ")  //while we put 2 space it store as 2 words if we put 1 space it wil store as strins as all letters seperatlty
-  let firstName=k[0].charAt(0).toUpperCase()
-  let lastName=k[1].charAt(0).toUpperCase()
-  return `${firstName}.${lastName};`
-}
-console.log(abbrevName("subhin Akshaya"));
+// function abbrevName(name){
+//   let k= name.split("")  //while we put 1 space it store as 2 words if we put no space it wil store as strins as all letters seperatlty
+//   let firstName=k[0].charAt(0).toUpperCase()
+//   let lastName=k[1].charAt(0).toUpperCase()
+//   return `${firstName}.${lastName};`
+// }
+// console.log(abbrevName("subhin Akshaya"));
  
+//.20. Given an array of integers your solution should find the smallest integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
+
+let k=[2,3,4,1,-55,777,-888]
+
+ function findSmallestInt(args) {
+   
+  }
+
+  console.log(findSmallestInt(k));
 
