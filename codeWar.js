74fcea0411c407c528bbfdaeeadb584a2ab10390
SUBHIN-TM <subhintm2018@gmail.com>
2023-12-12 20:37:00 +0530

    52

--- codeWar.js
+++ codeWar.js
@@ -736,9 +736,28 @@ FUNDAMENTALS*/
 'word'   =>  'drow'
 */
 
-function solution(str){
-return  str.split("").reverse().join("")
+// function solution(str){
+// return  str.split("").reverse().join("")
   
-}
+// }
 
-console.log(solution("hello"));
+// console.log(solution("hello"));
+
+
+/*52.Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  if(input ===null || input.length==0){
+    return []
+  }else{
+    let positve= input.filter((data) => data > 0 ).length
+    let negative=input.filter((data) => data <0  ).reduce((total,data) => total+data ,0)
+    return [positve,negative]
+  }
+  }
+
+console.log(countPositivesSumNegatives([]));
