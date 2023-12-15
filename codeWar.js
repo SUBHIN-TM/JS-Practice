95f5328e8c07e3756f81f55df78b66dba9da81e0
SUBHIN-TM <subhintm2018@gmail.com>
2023-12-15 09:30:28 +0530

    54

--- codeWar.js
+++ codeWar.js
@@ -775,3 +775,19 @@ function filter_list(l) {
 }
 
 console.log(filter_list([1,2,"aasf","1","123",123]));
+
+
+
+/*54.Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+number of hotdogs	price per unit (cents)
+n < 5	100
+n >= 5 and n < 10	95
+n >= 10	90
+You can use if..else or ternary operator to complete it.
+When you have finished the work, click "Run Tests */
+
+function saleHotdogs(n){
+  return n<5 ? n*100 : n>=5 && n<10 ? n*95 :n*90
+}
+
+console.log(saleHotdogs(4))
