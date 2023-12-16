40fefadfa777f47a729e78f7fcf3d887794325d2
SUBHIN-TM <subhintm2018@gmail.com>
2023-12-16 09:44:16 +0530

    59

--- codeWar.js
+++ codeWar.js
@@ -841,8 +841,24 @@ Examples
 Example
 ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
  */
-function smash (words) {
- return words.join(" ")
-};
+// function smash (words) {
+//  return words.join(" ")
+// };
 
-console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+// console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+
+
+/*59.Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+For example:
+a = 1
+b = 4
+--> [1, 2, 3, 4] */
+function between(a, b) {
+  let result=[]
+ for(let i=a;i<=b;i++){
+  result.push(i)
+ }
+ return result
+}
+
+console.log(between(1,4));
