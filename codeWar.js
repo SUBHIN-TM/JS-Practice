370cd3dcf31d9370053af205b402ba31b473fa37
SUBHIN-TM <subhintm2018@gmail.com>
2023-12-27 09:55:29 +0530

    73

--- codeWar.js
+++ codeWar.js
@@ -1054,8 +1054,32 @@ Note: only positive integers will be tested.
 https://en.wikipedia.org/wiki/Triangle
 */
 
-function otherAngle(a, b) {
-  return 180-(a+b)
+// function otherAngle(a, b) {
+//   return 180-(a+b)
+// }
+
+// console.log(otherAngle(30,60));
+
+
+
+/*73.You might know some pretty large perfect squares. But what about the NEXT one?
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+Examples:(Input --> Output)
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+*/
+
+function findNextSquare(sq) {
+ let number= Math.sqrt(sq)
+ if(Number.isInteger(number)){ //integer contains both negative and positive number and zero also but not contain decimal values
+  return Math.pow(number+1,2)
+ }else{
+  return -1
+ }
 }
 
-console.log(otherAngle(30,60));
+console.log(findNextSquare(114));
+
+
