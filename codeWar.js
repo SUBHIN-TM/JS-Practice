675a4b8daf043be6870104b3f2b36016863a5b0c
SUBHIN-TM <subhintm2018@gmail.com>
2023-12-28 09:54:06 +0530

    75

--- codeWar.js
+++ codeWar.js
@@ -1096,3 +1096,27 @@ const quarterOf = (month) => {
   return Math.ceil(month/3) //CEIL ALWAYS ROUND TO AFTER THE POINT 1.22 =2
 }
 console.log(quarterOf(12));
+
+
+/*75.Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+Examples:
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.*/
+function roundToNextMultipleOf5(number) {
+  if (number % 5 === 0) {
+      return number;
+  } else {
+      // Calculate the next multiple of 5
+      return Math.ceil(number / 5) * 5; //MATH CEIL ONLY FOR THE BRACKET
+  }
+}
+
+console.log(roundToNextMultipleOf5(7));
