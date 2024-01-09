81e1a4baa3466dd4227a9199f60bc6123946b5f8
SUBHIN-TM <subhintm2018@gmail.com>
2024-01-09 10:22:34 +0530

    90

--- codeWar.js
+++ codeWar.js
@@ -1447,9 +1447,10 @@ Examples (Input --> Output)
 ""  -->  ""
 */
 
-function order(words){
- 
-  return words.split(" ")
+function order(words) {
+ return words.split(" ").sort((a,b) => a.match(/\d/) - b.match(/\d/)).join(" ")
+   
 }
 
 console.log(order("is2 Thi1s T4est 3a"));
+
