0536be1007f6b5225d627b120a23d17286299b29
SUBHIN-TM <subhintm2018@gmail.com>
2024-01-22 09:59:53 +0530

    107

--- codeWar.js
+++ codeWar.js
@@ -1834,18 +1834,113 @@ For example, when given size is 3:
 For the given example, the return value should be:
 [[1,2,3],[2,4,6],[3,6,9]] */
 
-multiplicationTable = function(size) {
-  let last=[]
-  for(let j=1;j<=size;j++){
-    let result=[]
-    for(let i=1;i<=size;i++){
-      result.push(i*j)
-     }
+// multiplicationTable = function(size) {
+//   let last=[]
+//   for(let j=1;j<=size;j++){
+//     let result=[]
+//     for(let i=1;i<=size;i++){
+//       result.push(i*j)
+//      }
 
-    last.push(result)
+//     last.push(result)
+//   }
+
+// return last
+// }
+
+// console.log(multiplicationTable(3));
+
+/*106.Once upon a time, on a way through the old wild mountainous west,…
+… a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+How I crossed a mountainous desert the smart way.
+The directions given to the man are, for example, the following (depending on the language):
+
+["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+or
+{ "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+or
+[North, South, South, East, West, North, West]
+You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+["WEST"]
+or
+{ "WEST" }
+or
+[West]
+Other examples:
+In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+
+In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
+Task
+Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+The Haskell version takes a list of directions with data Direction = North | East | West | South.
+The Clojure version returns nil when the path is reduced to nothing.
+The Rust version takes a slice of enum Direction {North, East, West, South}.
+See more examples in "Sample Tests:"
+Notes
+Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+if you want to translate, please ask before translating. */
+
+// function dirReduc(arr) {
+//   const opposites = { "NORTH": "SOUTH", "SOUTH": "NORTH", "EAST": "WEST", "WEST": "EAST" };
+//   const result = [];
+
+//   for (const direction of arr) {
+//     if (result.length && result[result.length - 1] === opposites[direction]) {
+//       result.pop(); // Remove the last element if it's opposite to the current direction
+//     } else {
+//       result.push(direction);
+//     }
+//   }
+
+//   return result;
+// }
+
+// // Example usage:
+// const directions = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"];
+// const result = dirReduc(directions);
+// console.log(result);
+
+
+
+/*107.The number 
+89
+89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 
+89
+=
+8
+1
++
+9
+2
+89=8
+
+      assert.deepEqual(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+      assert.deepEqual(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
+      assert.deepEqual(sumDigPow(10, 100),  [89])
+      assert.deepEqual(sumDigPow(90, 100), [])
+*/
+
+function sumDigPow(a,b) {
+
+  let result=[]
+  for (let i = a; i <= b; i++) {
+    let num=i.toString().split("")
+    let sum=num.map((data,index) => {
+      return Math.pow(Number(data),index+1)
+  }).reduce((acc,data) => acc+ data ,0)
+    if(sum === i){
+      result.push(i)
+    }
+    
   }
-
-return last
+return result
 }
 
-console.log(multiplicationTable(3));
+console.log(sumDigPow(1, 10));
