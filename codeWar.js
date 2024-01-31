54a482023a0d1b6c08a7f3375880f0ae72f161a1
SUBHIN-TM <subhintm2018@gmail.com>
2024-01-31 10:01:50 +0530

    116

--- codeWar.js
+++ codeWar.js
@@ -2170,24 +2170,84 @@ Example
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice */
 
-function duplicateCount(text){
-  let array=[...text]
-  let count ={}
-  let totalCount=[]
-  let small=array.map((data) => data.toLowerCase())
-  for(let i=0;i<small.length;i++){
-    if(count[small[i]]){
-      count[small[i]] +=1;
-    }else{
-      count[small[i]] =1;
-    }
-  }
+// function duplicateCount(text){
+//   let array=[...text]
+//   let count ={}
+//   let totalCount=[]
+//   let small=array.map((data) => data.toLowerCase())
+//   for(let i=0;i<small.length;i++){
+//     if(count[small[i]]){
+//       count[small[i]] +=1;
+//     }else{
+//       count[small[i]] =1;
+//     }
+//   }
  
-  for(let key in count){
-    if(count[key] > 1){
-      totalCount.push(count[key])
+//   for(let key in count){
+//     if(count[key] > 1){
+//       totalCount.push(count[key])
+//     }
+//   }
+//   return totalCount.length
+// }
+// console.log(duplicateCount('aabBcdeB'));
+
+
+/*115.In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+"a" = 1, "b" = 2, etc.
+Example
+(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+*/
+
+// function alphabetPosition(text) {
+//  let output=[]
+//  for (let i = 0; i < text.length; i++) {
+//     const character=text[i].toLowerCase();
+//     if(character >= 'a' && character<= 'z'){
+//       const position=character.charCodeAt(0) - 96
+//       output.push(position)
+//     }
+//  }
+//  return output.join(" ")
+// }
+
+// console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+
+/*116.Well met with Fibonacci bigger brother, AKA Tribonacci.
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...
+Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+  doTest([ [0,0,1], 10], [0,0,1,1,2,4,7,13,24,44]     )
+  doTest([ [0,1,1], 10], [0,1,1,2,4,7,13,24,44,81]    )
+  doTest([ [1,0,0], 10], [1,0,0,1,1,2,4,7,13,24]      )
+  doTest([ [0,0,0], 10], [0,0,0,0,0,0,0,0,0,0]        )
+  doTest([ [1,2,3], 10], [1,2,3,6,11,20,37,68,125,230])
+  doTest([ [3,2,1], 10], [3,2,1,6,9,16,31,56,103,190] )
+  doTest([ [1,1,1],  1], [1])
+  doTest([[300,200,100], 0], []) */
+
+  function tribonacci(signature,n){
+    if(n ==0 || n==null){
+      return []
     }
+    let array=[...signature]
+    for (let i = 0; i < n - 3; i++) {
+    let trys=array[i] + array[i+1] + array[i+2]
+    array.push(trys)
+    }
+    if(n<signature.length){
+    return array.splice(0,n)
+    }
+    return array
   }
-  return totalCount.length
-}
-console.log(duplicateCount('aabBcdeB'));
+  console.log(tribonacci([1,1,1], 2));
