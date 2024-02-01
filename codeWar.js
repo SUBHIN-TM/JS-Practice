4646ae063c8bdb1470d3c9f858af1b19876cb982
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-01 09:43:57 +0530

    117

--- codeWar.js
+++ codeWar.js
@@ -2236,18 +2236,53 @@ If you enjoyed this kata more advanced and generalized version of it can be foun
   doTest([ [1,1,1],  1], [1])
   doTest([[300,200,100], 0], []) */
 
-  function tribonacci(signature,n){
-    if(n ==0 || n==null){
-      return []
+  // function tribonacci(signature,n){
+  //   if(n ==0 || n==null){
+  //     return []
+  //   }
+  //   let array=[...signature]
+  //   for (let i = 0; i < n - 3; i++) {
+  //   let trys=array[i] + array[i+1] + array[i+2]
+  //   array.push(trys)
+  //   }
+  //   if(n<signature.length){
+  //   return array.splice(0,n)
+  //   }
+  //   return array
+  // }
+  // console.log(tribonacci([1,1,1], 2));
+
+
+
+
+
+  /*117.Enough is enough! DELETING DUPLICATES MORE THAN A NUMBER OCUURANCE TIMES ,WITHOUT CHANGING THE ORDER OF ARRAY`
+Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
+He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+Task
+Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+With list [20,37,20,21] and number 1, the result would be [20,37,21]. */
+
+function deleteNth(arr, n) {
+  let count = {};
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+
+    if (count[current]) {
+      count[current] += 1;
+      if (count[current] <= n) {
+        result.push(current);
+      }
+    } else {
+      count[current] = 1;
+      result.push(current);
     }
-    let array=[...signature]
-    for (let i = 0; i < n - 3; i++) {
-    let trys=array[i] + array[i+1] + array[i+2]
-    array.push(trys)
-    }
-    if(n<signature.length){
-    return array.splice(0,n)
-    }
-    return array
   }
-  console.log(tribonacci([1,1,1], 2));
+
+  return result;
+}
+
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3));
