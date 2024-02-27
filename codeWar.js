4ea5a72e9143ede03da990b250b2465aa8beb21f
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-27 10:17:40 +0530

    131

--- codeWar.js
+++ codeWar.js
@@ -2741,8 +2741,41 @@ Even-odd disparity */
 7 returns "Saturday"
 Otherwise returns "Wrong, please enter a number between 1 and 7" */
 
-function whatday(num) { 
-let defined={1:'Sunday',2:'Monday',3:'Tuesday',4:'Wednesday',5:'Thursday',6:'Friday',7:'Saturday'}
-return defined[num] ? defined[num]:'Wrong, please enter a number between 1 and 7'
+// function whatday(num) { 
+// let defined={1:'Sunday',2:'Monday',3:'Tuesday',4:'Wednesday',5:'Thursday',6:'Friday',7:'Saturday'}
+// return defined[num] ? defined[num]:'Wrong, please enter a number between 1 and 7'
+// }
+// console.log(whatday(1));
+
+
+/*131.Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+foo -> foo1
+fobar23 -> foobar24
+foo0042 -> foo0043
+foo9 -> foo10
+foo099 -> foo100
+Attention: If the number has leading zeros the amount of digits should be considered. */
+
+
+function incrementString (strng) {
+  const regex = /(\d*)$/;
+  let numberPart=strng.match(regex)[0]//it wil l return arry contain more details at the 0 indeex giv4 the exact numbers
+  if(!numberPart){
+    return strng+1
+  }else{
+   let numberLength=numberPart.length
+   let incremented=parseInt(numberPart, 10) + 1
+   let incrementedStr = incremented.toString();
+   let zerosNeeded = numberLength - incrementedStr.length;
+
+   let leadingZeros = zerosNeeded > 0 ? '0'.repeat(zerosNeeded) : '';
+   let replaced= strng.slice(0,-numberLength)
+   return replaced + leadingZeros + incrementedStr;
+   
+  }
 }
-console.log(whatday(1));
+console.log(incrementString('foo0042'));
