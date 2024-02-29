dbf21f8c2fa7cbfb586fa44ad16c57d633a72978
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-29 09:28:26 +0530

    136

--- codeWar.js
+++ codeWar.js
@@ -2831,17 +2831,41 @@ Examples:
 
 
 
-// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//135. Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 // Examples (input -> output)
 // 6, "I"     -> "IIIIII"
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
-function repeatStr (n, s) {
-  let result=""
- for(let i=0;i<n;i++){
-   result +=s
- }
-  return result
-}
+// function repeatStr (n, s) {
+//   let result=""
+//  for(let i=0;i<n;i++){
+//    result +=s
+//  }
+//   return result
+// }
 
-console.log(repeatStr(3,"ha "));
+// console.log(repeatStr(3,"ha "));
+
+
+
+
+// 136 .Your task is to sum the differences between consecutive pairs in the array in descending order.
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+// function sumOfDifferences(arr) {
+//   let result=0
+//   let sorted=arr.sort((a,b) => b-a)
+//   for(let i=0;i<sorted.length-1;i++){
+//     result += sorted[i] - sorted[i+1]
+//   }
+//   return result
+  
+// }
+
+// console.log(sumOfDifferences([1,2,10]));
+
+
