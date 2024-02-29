eaa9e500fb386a25e01086d6a3654841a4cde177
SUBHIN-TM <subhintm2018@gmail.com>
2024-02-29 09:14:15 +0530

    135

--- codeWar.js
+++ codeWar.js
@@ -2816,15 +2816,32 @@ Examples:
 // replace("aeiou") === "!!!!!"
 // replace("ABCDE") === "!BCD!"
 
-function replace(s){
+// function replace(s){
+//   let result=""
+// for(let i=0;i<s.length;i++){
+//   if('AEIOUaeiou'.includes(s[i])){
+//      result +="!"
+//      }else{
+//     result +=s[i]
+//   }
+// }
+// return result
+// }
+// console.log(replace("KbmjJa Xhu KBP oNXOAt WDfL iFXIe TPkcwCZ wTAC"));
+
+
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
   let result=""
-for(let i=0;i<s.length;i++){
-  if('AEIOUaeiou'.includes(s[i])){
-     result +="!"
-     }else{
-    result +=s[i]
-  }
+ for(let i=0;i<n;i++){
+   result +=s
+ }
+  return result
 }
-return result
-}
-console.log(replace("KbmjJa Xhu KBP oNXOAt WDfL iFXIe TPkcwCZ wTAC"));
+
+console.log(repeatStr(3,"ha "));
